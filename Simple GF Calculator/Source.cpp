f976a93956beef663067add25659e61aff8f78f5
mullerpoint <gary@mullerpoint.com>
2016-02-22 12:25:20 -0700

    working build, multiplication is now a table lookup

--- Simple GF Calculator/Source.cpp
+++ Simple GF Calculator/Source.cpp
@@ -7,8 +7,7 @@
 // Copyright 2016 Gary Muller
 //
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// A simple four-function calculator in GF(2^4). Uses table lookups for the
-// multiplicative inverses.
+// A simple four-function calculator in GF(2^4). Can use table lookups
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
@@ -17,12 +16,14 @@
 #include <iostream> //default include for functionality
 #include <string> //get working string functionality
 #include <bitset> //print binary
+#include <algorithm> //arrays searching
 #endif
 
 //Global variables and defines
+
 std::bitset<4> galiosField16[16] =
 {
-	std::bitset<4>("0000"),
+	std::bitset<4>("0000"), 
 	std::bitset<4>("0001"),
 	std::bitset<4>("0010"),
 	std::bitset<4>("0011"),
@@ -39,6 +40,73 @@ std::bitset<4> galiosField16[16] =
 	std::bitset<4>("1110"),
 	std::bitset<4>("1111")
 };
+std::bitset<4> GFMultTable[16][16] =  //multiplication table for GF(2^4) mod(2)
+{
+	/*01*/{ std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"),
+	std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"),
+	std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"),
+	std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000"), std::bitset<4>("0000") },
+	/*02*/{ std::bitset<4>("0000"), std::bitset<4>("0001"), std::bitset<4>("0010"), std::bitset<4>("0011"),
+	std::bitset<4>("0100"), std::bitset<4>("0101"), std::bitset<4>("0110"), std::bitset<4>("0111"),
+	std::bitset<4>("1000"), std::bitset<4>("1001"), std::bitset<4>("1010"), std::bitset<4>("1011"),
+	std::bitset<4>("1100"), std::bitset<4>("1101"), std::bitset<4>("1110"), std::bitset<4>("1111") },
+	/*03*/{ std::bitset<4>("0000"), std::bitset<4>("0010"), std::bitset<4>("0100"), std::bitset<4>("0110"),
+	std::bitset<4>("1000"), std::bitset<4>("1010"), std::bitset<4>("1100"), std::bitset<4>("1110"),
+	std::bitset<4>("0011"), std::bitset<4>("0001"), std::bitset<4>("0111"), std::bitset<4>("0101"),
+	std::bitset<4>("1011"), std::bitset<4>("1001"), std::bitset<4>("1111"), std::bitset<4>("1101") },
+	/*04*/{ std::bitset<4>("0000"), std::bitset<4>("0011"), std::bitset<4>("0110"), std::bitset<4>("0101"),
+	std::bitset<4>("1100"), std::bitset<4>("1111"), std::bitset<4>("1010"), std::bitset<4>("1001"),
+	std::bitset<4>("1011"), std::bitset<4>("1000"), std::bitset<4>("1101"), std::bitset<4>("1110"),
+	std::bitset<4>("0111"), std::bitset<4>("0100"), std::bitset<4>("0001"), std::bitset<4>("0010") },
+	/*05*/{ std::bitset<4>("0000"), std::bitset<4>("0100"), std::bitset<4>("1000"), std::bitset<4>("1100"),
+	std::bitset<4>("0011"), std::bitset<4>("0111"), std::bitset<4>("1011"), std::bitset<4>("1111"),
+	std::bitset<4>("0110"), std::bitset<4>("0010"), std::bitset<4>("1110"), std::bitset<4>("1010"),
+	std::bitset<4>("0101"), std::bitset<4>("0001"), std::bitset<4>("1101"), std::bitset<4>("1001") },
+	/*06*/{ std::bitset<4>("0000"), std::bitset<4>("0101"), std::bitset<4>("1010"), std::bitset<4>("1111"),
+	std::bitset<4>("0111"), std::bitset<4>("0010"), std::bitset<4>("1101"), std::bitset<4>("1000"),
+	std::bitset<4>("1110"), std::bitset<4>("1011"), std::bitset<4>("0100"), std::bitset<4>("0001"),
+	std::bitset<4>("1001"), std::bitset<4>("1100"), std::bitset<4>("0011"), std::bitset<4>("0110") },
+	/*07*/{ std::bitset<4>("0000"), std::bitset<4>("0110"), std::bitset<4>("1100"), std::bitset<4>("1010"),
+	std::bitset<4>("1011"), std::bitset<4>("1101"), std::bitset<4>("0111"), std::bitset<4>("0001"),
+	std::bitset<4>("0101"), std::bitset<4>("0011"), std::bitset<4>("1001"), std::bitset<4>("1111"),
+	std::bitset<4>("1110"), std::bitset<4>("1000"), std::bitset<4>("0010"), std::bitset<4>("0100") },
+	/*08*/{ std::bitset<4>("0000"), std::bitset<4>("0111"), std::bitset<4>("1110"), std::bitset<4>("1001"),
+	std::bitset<4>("1111"), std::bitset<4>("1000"), std::bitset<4>("0001"), std::bitset<4>("0110"),
+	std::bitset<4>("1101"), std::bitset<4>("1010"), std::bitset<4>("0011"), std::bitset<4>("0100"),
+	std::bitset<4>("0010"), std::bitset<4>("0101"), std::bitset<4>("1100"), std::bitset<4>("1011") },
+	/*09*/{ std::bitset<4>("0000"), std::bitset<4>("1000"), std::bitset<4>("0011"), std::bitset<4>("1011"),
+	std::bitset<4>("0110"), std::bitset<4>("1110"), std::bitset<4>("0101"), std::bitset<4>("1101"),
+	std::bitset<4>("1100"), std::bitset<4>("0100"), std::bitset<4>("1111"), std::bitset<4>("0111"),
+	std::bitset<4>("1010"), std::bitset<4>("0010"), std::bitset<4>("1001"), std::bitset<4>("0001") },
+	/*10*/{ std::bitset<4>("0000"), std::bitset<4>("1001"), std::bitset<4>("0001"), std::bitset<4>("1000"),
+	std::bitset<4>("0010"), std::bitset<4>("1011"), std::bitset<4>("0011"), std::bitset<4>("1010"),
+	std::bitset<4>("0100"), std::bitset<4>("1101"), std::bitset<4>("0101"), std::bitset<4>("1100"),
+	std::bitset<4>("0110"), std::bitset<4>("1111"), std::bitset<4>("0111"), std::bitset<4>("1110") },
+	/*11*/{ std::bitset<4>("0000"), std::bitset<4>("1010"), std::bitset<4>("0111"), std::bitset<4>("1101"),
+	std::bitset<4>("1110"), std::bitset<4>("0100"), std::bitset<4>("1001"), std::bitset<4>("0011"),
+	std::bitset<4>("1111"), std::bitset<4>("0101"), std::bitset<4>("1000"), std::bitset<4>("0010"),
+	std::bitset<4>("0001"), std::bitset<4>("1011"), std::bitset<4>("1100"), std::bitset<4>("0110") },
+	/*12*/{ std::bitset<4>("0000"), std::bitset<4>("1011"), std::bitset<4>("0101"), std::bitset<4>("1110"),
+	std::bitset<4>("1010"), std::bitset<4>("0001"), std::bitset<4>("1111"), std::bitset<4>("0100"),
+	std::bitset<4>("0111"), std::bitset<4>("1100"), std::bitset<4>("0010"), std::bitset<4>("1001"),
+	std::bitset<4>("1101"), std::bitset<4>("0110"), std::bitset<4>("1000"), std::bitset<4>("0011") },
+	/*13*/{ std::bitset<4>("0000"), std::bitset<4>("1100"), std::bitset<4>("1011"), std::bitset<4>("0111"),
+	std::bitset<4>("0101"), std::bitset<4>("1001"), std::bitset<4>("1110"), std::bitset<4>("0010"),
+	std::bitset<4>("1010"), std::bitset<4>("0110"), std::bitset<4>("0001"), std::bitset<4>("1101"),
+	std::bitset<4>("1111"), std::bitset<4>("0011"), std::bitset<4>("0100"), std::bitset<4>("1000") },
+	/*14*/{ std::bitset<4>("0000"), std::bitset<4>("1101"), std::bitset<4>("1001"), std::bitset<4>("0100"),
+	std::bitset<4>("0001"), std::bitset<4>("1100"), std::bitset<4>("1000"), std::bitset<4>("0101"),
+	std::bitset<4>("0010"), std::bitset<4>("1111"), std::bitset<4>("1011"), std::bitset<4>("0110"),
+	std::bitset<4>("0011"), std::bitset<4>("0111"), std::bitset<4>("1010"), std::bitset<4>("0111") },
+	/*15*/{ std::bitset<4>("0000"), std::bitset<4>("1110"), std::bitset<4>("1111"), std::bitset<4>("0001"),
+	std::bitset<4>("1101"), std::bitset<4>("0011"), std::bitset<4>("0010"), std::bitset<4>("1100"),
+	std::bitset<4>("1001"), std::bitset<4>("0111"), std::bitset<4>("0110"), std::bitset<4>("1000"),
+	std::bitset<4>("0100"), std::bitset<4>("1010"), std::bitset<4>("1011"), std::bitset<4>("0101") },
+	/*16*/{ std::bitset<4>("0000"), std::bitset<4>("1111"), std::bitset<4>("1101"), std::bitset<4>("0010"),
+	std::bitset<4>("1001"), std::bitset<4>("0110"), std::bitset<4>("0100"), std::bitset<4>("1011"),
+	std::bitset<4>("0001"), std::bitset<4>("1110"), std::bitset<4>("1100"), std::bitset<4>("0011"),
+	std::bitset<4>("1000"), std::bitset<4>("0111"), std::bitset<4>("0101"), std::bitset<4>("1010") }
+};
 //
 
 //Function Prototypes
@@ -46,6 +114,7 @@ std::bitset<4> GFAdd(std::bitset<4>, std::bitset<4>);
 std::bitset<4> GFMult(std::bitset<4>, std::bitset<4>);
 std::bitset<4> GFDiv(std::bitset<4>, std::bitset<4>);
 std::bitset<4> findInverse(std::bitset<4>);
+int locationFind(std::bitset<4>);
 //
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -57,22 +126,25 @@ std::bitset<4> findInverse(std::bitset<4>);
 int main()
 {
 	//main variables
-	char operation; //what operation should be done
-	std::string string1, string2;
-	std::bitset<4> binaryResult; // what 
+		char operation; //what operation should be done
+		std::string string1, string2; // temporary string variables to read user inputs into
+		std::bitset<4> binaryResult; // variable for value returned to be saved in
+	
 	//query user for inputs
-		//ask user for operation
-		std::cout << "What operation would you like to perform (+, -, *, /): ";
-		std::cin >> operation;
 		//ask user for inputs on that operation
-		std::cout << "Enter the first input in a binary form : ";
+		std::cout << "Enter the first operand in a binary form : ";
 		std::cin >> string1;
 		std::bitset<4> binary1(string1);
 
-		std::cout << "Enter the second input in a binary form: ";
+		std::cout << "Enter the second operand in a binary form: ";
 		std::cin >> string2;
 		std::bitset<4> binary2(string2);
-		
+
+		//ask user for operation
+		std::cout << "What operation would you like to perform on these operands(+, -, *, /): ";
+		std::cin >> operation;
+
+
 	//perform the operation requested
 		switch (operation)
 		{
@@ -85,7 +157,14 @@ int main()
 			binaryResult = GFMult(binary1, binary2);
 			break;
 		case '/':
-			binaryResult = GFDiv(binary1, binary2);
+			if (binary2 == galiosField16[0])//if an input is zero(0) ie '0000' then division is impossible
+			{
+				binaryResult = NULL;
+			}
+			else
+			{
+				binaryResult = GFDiv(binary1, binary2);
+			}
 			break;
 		default:
 			std::cout << "try again using the operators shown" << std::endl;
@@ -107,28 +186,28 @@ std::bitset<4> GFAdd(std::bitset<4> input1, std::bitset<4> input2) //adding is t
 
 std::bitset<4> GFMult(std::bitset<4> input1, std::bitset<4> input2)
 {
-	std::bitset<8> input1big(input1.to_string()); //expand the inputs to allow for calculations
-	std::bitset<8> input2big(input2.to_string());
-	std::bitset<8> primitive("10011");	// the primitive/irreducible polynomial function in the GF(2^4) field
-	std::bitset<8> one("0001");		// one(1) in binary 
-	std::bitset<8> maxbits("1111");	// sixteen(16) in binary / the max value that can be stored in 4 bits
-	std::bitset<8> productbig("0000");	// the eventual product of the multiplication 
+	//declare local variables
+		int x, y; //the x,y position of the answer the table
 
-	while (input2big.to_ulong()) {
-		if ((input2big & one).to_ulong() == 1) // if b is odd, then add the corresponding a to p (final product = sum of all a's corresponding to odd b's)
-			productbig ^= input1big;				// since we're in GF(2^m), addition is an XOR
+	//perform calculations
+		x = locationFind(input1);
+		y = locationFind(input2);
 
-		if ((input1big & maxbits).to_ulong() >= 16)	// GF modulo: if input1 >= 16, then it will overflow when shifted left, so reduce
-			input1big = (input1big << 1) ^ primitive;		// XOR with the primitive polynomial x^4 + x + 1 
-		else
-			input1big <<= 1;	// equivalent to a*2
-		input2big >>= 1;	// equivalent to b // 2
-	}
-	std::bitset<4> product((productbig.to_string()).erase(4));
-	return product;
+	//return results
+		return GFMultTable[x][y];
 
 }
 
+
+int locationFind(std::bitset<4> searchBits)
+{
+	int count = 0;
+	while (galiosField16[count] != searchBits)
+	{
+		count++;
+	}
+	return count;
+}
 // http://web.eecs.utk.edu/~plank/plank/papers/CS-07-593/
 // http://mathworld.wolfram.com/IrreduciblePolynomial.html
 // http://www.cprogramming.com/tutorial/bitwise_operators.html
@@ -138,25 +217,33 @@ std::bitset<4> GFMult(std::bitset<4> input1, std::bitset<4> input2)
 
 std::bitset<4> GFDiv(std::bitset<4> input1, std::bitset<4> input2) //division is the same as multiplying by the inverse
 {
-	std::bitset<4> result;
-	std::bitset<4> inverse;
+	//declare local variables
+		std::bitset<4> result;
+		std::bitset<4> inverse;
 	
-	inverse = findInverse(input1);
-	result = GFMult(inverse, input2);
+	//perform calculations
+		inverse = findInverse(input2); //find the inverse
+
+		result = GFMult(input1, inverse); //multiply the  first operand, and inverse of the second operand to get the answer
 	
-	return result;
+	//return results
+		return result;
 }
 
-std::bitset<4> findInverse(std::bitset<4> input1) //brute force inverse finder
+std::bitset<4> findInverse(std::bitset<4> input) //brute force inverse finder
 {
-	std::bitset<4> product("0000"); // create and initialize the return product
-	std::bitset<4> comparison("0001"); //one in binary
-	int count = 0;
+	//declare local variables
+		std::bitset<4> product("0000"); // create and initialize the return product
+		std::bitset<4> comparison("0001"); //one in binary
+		int count = 1; //skip 0
 
+	//perform calculations
 	while (product != comparison) //we know we found the inverse when the result is '0001' or one(1)
 	{
-		product = GFMult(input1, galiosField16[count]);
+		product = GFMult(input, galiosField16[count]); //try every possible number
 		count++;
 	}
-	return product;
+
+	//return results
+	return galiosField16[count-1]; // remove one(1) from count to get the right answer
 }
